e9921aaa859f4c4d2ded2a2297aecff91782d881
Alireza Askarpour <askarpourdev@gmail.com>
2023-06-18 06:52:20 -0400

    feat: add morgan middleware

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,7 +2,9 @@ import cors from "cors"
 import dotenv from "dotenv"
 import createError from "http-errors"
 import swaggerUI from "swagger-ui-express"
-import express, { NextFunction, Request, Response } from "express"
+import express, { NextFunction, Request } from "express"
+
+import { morganMiddleware } from "./middlewares/morgan.middleware"
 
 import { swaggerSetup } from "./config/swagger.config"
 import { appListener, globalErrorHandler, port } from "./config/app.config"
@@ -15,6 +17,7 @@ dotenv.config()
 const app = express()
 
 // middlewares
+app.use(morganMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static("uploads"))
@@ -29,7 +32,7 @@ app.use(cors({ origin: "*" }))
 app.use(allRoutes)
 
 // error handler
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req: Request, _, next: NextFunction) => {
   next(createError.NotFound(`Can't find ${req.originalUrl} on the server!`))
 })
 app.use(globalErrorHandler)
